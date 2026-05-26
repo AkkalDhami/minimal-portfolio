26f30204c5e81a012125b84dc42a543a73697bcb
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-26 16:13:23 +0545

    feat: add DSA data type and initial dataset for Data Structures & Algorithms

---
+++ data/dsa.ts
@@ -0,0 +1,35 @@
+export type IDSA = {
+  slug: string;
+  title: string;
+  description: string;
+  docs: string;
+};
+
+export const DSA_DATA: IDSA[] = [
+  {
+    slug: "intro",
+    title: "01. Introduction to Data Structures & Algorithms",
+    description:
+      "Learn the basics of data structures and algorithms with this introduction.",
+    docs: "/docs/dsa/intro"
+  },
+  {
+    slug: "cpp-intro",
+    title: "02. Introduction to C++",
+    description:
+      "Learn the basics of C++. Variables, Data types, Control flow, and operators, etc.",
+    docs: "/docs/dsa/cpp-intro"
+  },
+  {
+    slug: "pattern-printing",
+    title: "03. Pattern Printing",
+    description: "Learn to print patterns in C++.",
+    docs: "/docs/dsa/pattern-printing"
+  },
+  {
+    slug: "no-system-conversion",
+    title: "04. Number System Conversion",
+    description: "Learn about converting between different number systems.",
+    docs: "/docs/dsa/no-system-conversion"
+  }
+];
