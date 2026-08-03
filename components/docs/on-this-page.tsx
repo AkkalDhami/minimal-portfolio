e8517d0807f3ec9b4b55dc36d3e394c23bb0d800
AkkalDhami <dhamiakkal21@gmail.com>
2026-08-03 18:50:08 +0545

    feat(on-this-page): filter out toc-ignore headings
    add public/images/dns2.png image asset

--- components/docs/on-this-page.tsx
+++ components/docs/on-this-page.tsx
@@ -55,8 +55,8 @@ export function OnThisPage({ docsRef }: Props) {
 
     const slugger = new GithubSlugger();
 
-    const elements = Array.from(
-      container.querySelectorAll("h2, h3")
+    const elements = Array.from(container.querySelectorAll("h2, h3")).filter(
+      el => !el.classList.contains("toc-ignore")
     ) as HTMLHeadingElement[];
 
     const list: Heading[] = elements
